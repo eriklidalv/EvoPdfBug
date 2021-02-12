fed3487e51b0916dfc5193aebd6924fea2799ee2
eriklidalv <erik@lidalv.se>
2021-02-12 10:48:01 +0100

    initial commit

---
+++ EvoPdfBug/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using EvoWordToPdf;
+
+namespace EvoPdfBug
+{
+    class Class1
+    {
+        public byte[] GetByteArrayAsync()
+        {
+            string dir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string pathSource = $"{dir}\\..\\..\\..\\error.pdf";
+
+            // https://docs.microsoft.com/en-us/dotnet/api/system.io.filestream.read?view=net-5.0
+            using (var fsSource = new FileStream(pathSource, FileMode.Open, FileAccess.Read))
+            {
+                // Read the source file into a byte array.
+                byte[] bytes = new byte[fsSource.Length];
+                int numBytesToRead = (int)fsSource.Length;
+                int numBytesRead = 0;
+                while (numBytesToRead > 0)
+                {
+                    // Read may return anything from 0 to numBytesToRead.
+                    int n = fsSource.Read(bytes, numBytesRead, numBytesToRead);
+
+                    // Break when the end of the file is reached.
+                    if (n == 0)
+                        break;
+
+                    numBytesRead += n;
+                    numBytesToRead -= n;
+                }
+                
+                return bytes;
+            }
+        }
+
+        public Document CreateDocument(byte[] byteArray, bool forcePortrait = true)
+        {
+            using (var stream = new MemoryStream(byteArray))
+            {
+                try
+                {
+                    var document = new Document(stream); // <- ERROR
+
+                    if (forcePortrait)
+                    {
+                        for (var i = 0; i < document.Pages.Count; i++)
+                        {
+                            var pdfPage = document.Pages[i];
+                            if (pdfPage.Orientation == PdfPageOrientation.Landscape && pdfPage.RotationAngle == 0)
+                            {
+                                pdfPage.Orientation = PdfPageOrientation.Portrait;
+                                pdfPage.RotationAngle = RotationAngle.Rotate_90;
+                            }
+                        }
+                    }
+
+                    return document;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error: {e}");
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ EvoPdfBug/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EvoPdfBug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var c = new Class1();
+            var bytes = c.GetByteArrayAsync();
+            var doc = c.CreateDocument(bytes);
+
+            Console.WriteLine(doc != null ? "Success" : "Fail");
+        }
+    }
+}
